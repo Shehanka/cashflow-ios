34622d60fe37770c97110613af405598fa655624
Shehanka <hcsperera@outlook.com>
2022-07-23 11:44:04 +0530

    Initial Commit

---
+++ CashflowUITests/CashflowUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CashflowUITestsLaunchTests.swift
+//  CashflowUITests
+//
+//  Created by Chamod Shehanka Perera on 2022-07-23.
+//
+
+import XCTest
+
+class CashflowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
